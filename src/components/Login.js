6ef10f1a2ea413e69a4cc68ee06f864410bd4497
joysanks <saikiajoysankar@gmail.com>
2026-07-26 23:16:35 +0530

    signIn/SignUp form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [signIn, setSignIn] = useState(true);
+  const toggleSignInForm = () => {
+    setSignIn(!signIn);
+  };
+
   return (
     <div>
       <Header />
@@ -12,11 +17,50 @@ const Login = () => {
         />
       </div>
 
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="text-3xl m-2 p-2">Sign In</h1>
-        <input type="email" placeholder="Email Address" className="p-2 m-2" />
-        <input type="password" placeholder="Password" className="p-2 m-2" />
-        <button className="p-2 m-2 bg-red-700  ">Sign In</button>
+      <form className="w-3/12 absolute p-8 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="text-3xl m-2 py-4">{signIn ? "Sign In" : "Sign Up"}</h1>
+        {!signIn &&<input
+          type="text"
+          placeholder="Full Name"
+          className="p-3 my-4 w-full bg-gray-700"
+        />}
+         <input
+          type="email"
+          placeholder="Email Address"
+          className="p-3 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 my-4 w-full bg-gray-700"
+        />
+        <button className="p-3 my-4 bg-red-700 w-full rounded-lg">
+          {signIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="my-2 p-2">
+          {signIn ? (
+            <>
+              New to Netflix?{" "}
+              <button
+                className="to-blue-500 underline"
+                onClick={toggleSignInForm}
+              >
+                Sign Up
+              </button>{" "}
+              Now
+            </>
+          ) : (
+            <>
+              Already Have an account?{" "}
+              <span
+                className="to-blue-500 underline cursor-pointer"
+                onClick={toggleSignInForm}
+              >
+                Sign In
+              </span>
+            </>
+          )}
+        </p>
       </form>
     </div>
   );
