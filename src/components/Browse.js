53a2d8c012124f02f377ba569bbcdd83b58068a2
joysanks <saikiajoysankar@gmail.com>
2026-08-01 15:23:11 +0530

    pushed nowPlayingMovies to redux store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import {addNowPlayingMovies} from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
   const url = "https://api.themoviedb.org/3/movie/now_playing?page=1";
 
   const getNowPlayingMovies = async () => {
     const data = await fetch(url, API_OPTIONS);
     const json = await data.json();
     console.log(json);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(()=>{
---
+++ src/utils/moviesSlice.js
@@ -0,0 +1,17 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const moviesSlice = createSlice({
+    name : 'movies',
+    initialState: {
+        nowPlayingMovies: null,
+    },
+    reducers: {
+        addNowPlayingMovies : (state, action) =>{
+            state.nowPlayingMovies = action.payload;
+        }
+    }
+})
+
+export const {addNowPlayingMovies} = moviesSlice.actions;
+
+export default moviesSlice.reducer;
