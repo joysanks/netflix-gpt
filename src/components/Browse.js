952235a917b7d5a759eb4e8f1800966d0d39a29d
joysanks <saikiajoysankar@gmail.com>
2026-08-03 00:30:44 +0530

    browse page building

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,28 +1,20 @@
-import React, { useEffect } from "react";
+
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import {addNowPlayingMovies} from "../utils/moviesSlice";
+// import { API_OPTIONS } from "../utils/constants";
+// import { useDispatch } from "react-redux";
+// import {addNowPlayingMovies} from "../utils/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-
-  const url = "https://api.themoviedb.org/3/movie/now_playing?page=1";
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(url, API_OPTIONS);
-    const json = await data.json();
-    console.log(json);
-    dispatch(addNowPlayingMovies(json.results));
-  };
-
-  useEffect(()=>{
-    getNowPlayingMovies();
-  },[])
+ useNowPlayingMovies();
 
   return (
     <div>
       <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
     </div>
   );
 };
