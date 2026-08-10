3e5513ec5f57c9f98b725715b50ca8cf5f59cf88
joysanks <saikiajoysankar@gmail.com>
2026-08-10 23:20:29 +0530

    style: enhance VideoBackground and VideoTitle components with responsive design and improved button styles

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[20%]  px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-4xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/3">{overview}</p>
       <div className="">
-        <button className="bg-gray-500 text-white py-2 px-10 w-30 text-lg  bg-opacity-50 rounded-lg">▶ Play</button>
+        <button className="bg-white text-black py-2 px-10 w-30 text-lg   rounded-lg hover:bg-opacity-80">▶ Play</button>
         <button className="mx-2 bg-gray-500 text-white py-2 px-10 w-30 text-lg  bg-opacity-50 rounded-lg">More Info</button>
       </div>
     </div>
